2d98d3c78fa43e00ed4ca8b85c806749c09b63ed
unknown <zxjonathanxz22@gmail.com>
2023-06-26 16:25:16 -0300

    organizando classe

--- src/Personagem.java
+++ src/Personagem.java
@@ -1,27 +1,29 @@
 public class Personagem {
+
     String nome;
     int energia;
+
     public Personagem(String nome,
-    int energia ) {
+            int energia) {
         this.nome = nome;
         this.energia = energia;
     }
-    
+
     int subtrairEnergia(int valor) {
         this.energia -= valor;
-        if(energia==0){
+        if (energia == 0) {
             System.out.println("Opa! Acabou sua energia! ");
         }
         return valor;
     }
-    void exibirEnergia(boolean fim){
-        if(fim){    
-        System.out.println("Sua energia final é de: "+this.energia);
+
+    void exibirEnergia(boolean fim) {
+        if (fim) {
+            System.out.println("Sua energia final é de: " + this.energia);
+        } else {
+            System.out.println("Sua energia é de: " + this.energia);
         }
-        else{
-            System.out.println("Sua energia é de: "+this.energia);
-        }
-            
+
     }
-    
+
 }
