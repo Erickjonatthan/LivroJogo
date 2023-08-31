25e5ea6be2400272094ea29977e178311016d662
ErickJonatthan <zxjonathanxz22@gmail.com>
2023-08-31 18:31:47 -0300

    Interface criada

--- src/Capitulo.java
+++ src/Capitulo.java
@@ -17,56 +17,32 @@ public class Capitulo {
         this.scan = scan;
         this.escolhas = new ArrayList<Escolha>();
         this.alteracaoEnergia = alteracaoEnergia;
-
     }
-
-    // private int escolher() {
-
-    //     if (this.escolhas.size() > 0) {
-        
-    //             for (int i = 0; i < this.escolhas.size(); i++) {
-    //                 if (.equalsIgnoreCase(this.escolhas.get(i).getTexto())) {
-    //                     return i;
-    //                 }
-    //             }
-              
-    //         }
-        
-    //     return -1;
-    // }
-
-
 
     public void setEscolha(String texto, Capitulo capitulo) {
 
         this.escolhas.add(new Escolha(texto, capitulo));
-
-
     }
+
     public void salvaProgresso(String proximaEscolha) {
 
-            this.personagem.setProgresso(proximaEscolha);
-            this.personagem.salvaArquivo();
-
-        
+        this.personagem.setProgresso(proximaEscolha);
+        this.personagem.salvaArquivo();        
     }
 
-
     public String getNome() {
+
         return this.nome;
     }
 
     public String getTexto() {
-        return this.texto.replace("PLAYER", personagem.getNome());
+        personagem.subtrairEnergia(alteracaoEnergia);
+        return this.texto.replace("PLAYER", personagem.getNome()) + "\n\n" + getMsg() ;
     }
 
     public String getMsg(){
-        personagem.subtrairEnergia(alteracaoEnergia);
+        
         String msg = "";
-        if (alteracaoEnergia != 0.0) {
-            msg = "Sua energia foi alterada em: "
-                    + String.format("%.0f", alteracaoEnergia * personagem.getEnergia());
-        }
         if (escolhas.size() > 0) {
             msg = "Energia: " + String.format("%.0f", personagem.getEnergia());
         }
@@ -74,6 +50,7 @@ public class Capitulo {
     }
 
     public ArrayList<Escolha> getEscolhas() {
+        
         return this.escolhas;
     }
 
--- src/LivroJogoController.java
+++ src/LivroJogoController.java
@@ -1,83 +1,118 @@
 import java.io.File;
 import java.util.HashMap;
 import java.util.Scanner;
-
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 public class LivroJogoController {
+    
+    @FXML
+    private AnchorPane buttonContainer;
 
     @FXML
-    private Button botaoCarregar;
-
-    @FXML
-    private Label textoCapitulo;
+    private Button botaoIniciar;
 
     @FXML
     private ButtonBar campoEscolhas;
 
     @FXML
+    private Label textoCapitulo;
+
+    @FXML
+    private Label campoImagem;
+    
+    Scanner scan = new Scanner(System.in, "UTF-8");
+    LeitorCarregador leitorCarregador = new LeitorCarregador();
+    HashMap<String, Personagem> personagens = leitorCarregador.lerPersonagens("rsc/personagens.txt");
+    HashMap<String, Capitulo> capitulosMap = leitorCarregador.lerCapitulos("rsc/capitulos.txt", personagens, scan);
+    File dados = new File("rsc/progresso.save");
+
+    @FXML
+    void initialize() {
+        criaBotao();
+    }
+    
+
+
+    @FXML
+    void iniciarNovoJogo(ActionEvent event) {
+        desativaBotao();
+        Capitulo raiz = capitulosMap.get("CAPITULO_IMAGEM 1");
+        dados.delete();
+        mostrar(raiz);
+        
+        
+    }
+    @FXML
     void carregarJogo(ActionEvent event) {
-        this.botaoCarregar.setVisible(false);
-
-        Scanner scan = new Scanner(System.in, "UTF-8");
-        LeitorCarregador leitorCarregador = new LeitorCarregador();
-        HashMap<String, Personagem> personagens = leitorCarregador.lerPersonagens("rsc/personagens.txt");
-        HashMap<String, Capitulo> capitulosMap = leitorCarregador.lerCapitulos("rsc/capitulos.txt", personagens, scan);
-        File dados = new File("rsc/progresso.save");
-
-
-        if (dados.exists()) {
-            Personagem personagemTemp = leitorCarregador.getPersonagemTemp(personagens);
-            Capitulo capituloTemp = capitulosMap.get(personagemTemp.getProgresso());
-            mostrar(capituloTemp);
-
-        } else {
-           Capitulo raiz = capitulosMap.get("CAPITULO_IMAGEM 1");
-           mostrar(raiz);
-
-        }
+        desativaBotao();
+        Personagem personagemTemp = leitorCarregador.getPersonagemTemp(personagens);
+        Capitulo capituloTemp = capitulosMap.get(personagemTemp.getProgresso());
+        mostrar(capituloTemp);
     }
 
+
+
+    private void desativaBotao() {
+        botaoIniciar.setVisible(false);
+        buttonContainer.setVisible(false);
+    }
+
+    
     private void mostrar(Capitulo capitulo){
 
         
         boolean isCapImagem = capitulo instanceof CapituloImagem;
         if(isCapImagem){
             CapituloImagem capituloImagem = (CapituloImagem) capitulo;
-        
-            this.textoCapitulo.setText(
-                capituloImagem.getImagem() +
-                capituloImagem.getTexto() +
-                "\n\n" +
-                capituloImagem.getMsg()
-
-                );
+            this.campoImagem.setText(capituloImagem.getImagem());
+            this.textoCapitulo.setText(capituloImagem.getTexto());
         }
         else{
             this.textoCapitulo.setText(capitulo.getTexto());
         }
-    
+        
         for (Escolha escolha : capitulo.getEscolhas()) {
             Button botao = new Button(escolha.getTexto());
+            botao.setPrefWidth(234);
+            botao.setPrefHeight(40);
+            botao.focusTraversableProperty().setValue(false);
             this.campoEscolhas.getButtons().add(botao);
             
            botao.setOnAction(new EventHandler<ActionEvent>() {
                @Override
                public void handle(ActionEvent event) {
-                campoEscolhas.getButtons().clear();
+                   campoEscolhas.getButtons().clear();
+                   campoImagem.setText("");
                 mostrar(escolha.getProximo());
                 capitulo.salvaProgresso(escolha.getNextCapituloNome());
                }
-           });
-           
-
-      
-         }
+            });
+        
+        }
     }
+   
 
+    private void criaBotao() {
+        if (dados.exists()) {
+            Button botao = new Button("Continuar Jogo");
+            botao.setPrefWidth(234); 
+            botao.setPrefHeight(60);
+            botao.focusTraversableProperty().setValue(false);
+            botao.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    carregarJogo(event);
+                }
+            });
+            this.buttonContainer.getChildren().add(botao);
+            buttonContainer.requestLayout();
+        }
+        }
+    
 }
