c78a3453d60c53adb39c1d2b313bf1039de17d4d
ErickJonatthan <zxjonathanxz22@gmail.com>
2023-08-30 23:35:54 -0300

    ok

---
+++ src/Controlador.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Controlador extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+      
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Layout.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("Jogo");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
